1a33710092968644ac3d0a763a6bcf7aa756d64a
SanjayKomma <sanjukomma1@gmail.com>
2026-07-17 20:12:15 +0530

    did some minute changes to the home page and completed

--- src/TopRated.jsx
+++ src/TopRated.jsx
@@ -31,7 +31,7 @@ export const TopRated = () => {
             <div className="pt-3 px-4">
                 <h2 className="font-bold text-white text-xl">Top Rated Movies</h2>
                 <div className="py-4">
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4">
                         {movies && movies.filter(movie=>{
                             const matchesYear = selectedYear ? movie.release_date?.startsWith(selectedYear):true;
                             const matchesGenre = selectedGenre ? movie.genre_ids?.map(String).includes(selectedGenre):true;
@@ -39,7 +39,7 @@ export const TopRated = () => {
                         }).map((movie)=>(
                             <div key={movie.id} className="w-full flex flex-col bg-gray-900 rounded-xl overflow-hidden hover:scale-110">
                                 <Link to={`/movie/${movie.id}`}>
-                                <img className="w-full h-72 object-cover" src={movie.poster_path ? `${Image_Base_Url}${movie.poster_path}`: "No Poster"} alt={movie.title}/>
+                                <img className="w-full h-80 object-cover" src={movie.poster_path ? `${Image_Base_Url}${movie.poster_path}`: "No Poster"} alt={movie.title}/>
                                  <div className="p-2">
                                     <h3 className="font-bold text-white text-sm">{movie.title}</h3>
                                     <span className="font-bold text-gray-500 text-xs">{movie.release_date ? movie.release_date.substring(0,4):"N/A"}</span>
@@ -53,7 +53,7 @@ export const TopRated = () => {
             <div className="pt-3 px-4">
                 <h2 className="font-bold text-white text-xl">Top Rated TV Shows</h2>
                 <div className="py-4">
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4">
                         {tvShows && tvShows.filter(tvShow=>{
                             const matchesYear = selectedYear ? tvShow.first_air_date?.startsWith(selectedYear):true;
                             const matchesGenre = selectedGenre ? tvShow.genre_ids?.map(String).includes(selectedGenre):true;
@@ -61,7 +61,7 @@ export const TopRated = () => {
                         }).map((tvShow)=>(
                             <div key={tvShow.id} className="w-full flex flex-col bg-gray-900 rounded-xl overflow-hidden hover:scale-110">
                                 <Link to={`/tv/${tvShow.id}`}>
-                                <img className="w-full h-72 object-cover" src={tvShow.poster_path ? `${Image_Base_Url}${tvShow.poster_path}`: "No Poster"} alt={tvShow.title}/>
+                                <img className="w-full h-80 object-cover" src={tvShow.poster_path ? `${Image_Base_Url}${tvShow.poster_path}`: "No Poster"} alt={tvShow.title}/>
                                  <div className="p-2">
                                     <h3 className="font-bold text-white text-sm">{tvShow.name}</h3>
                                     <span className="font-bold text-gray-500 text-xs">{tvShow.first_air_date ? tvShow.first_air_date.substring(0,4):"N/A"}</span>
